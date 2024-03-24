70f76aa69feab1f9146506c836c3dce6a85038f8
maarcf <marr.cairo@gmail.com>
2024-03-24 14:13:54 -0300

    feat(module-2): install exercise

---
+++ module-2/src/App.jsx
@@ -0,0 +1,15 @@
+
+
+import CharacterEditor from './components/CharacterEditor';
+import Footer from './components/Footer';
+
+function App() {
+  return (
+    <>
+      <CharacterEditor />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
