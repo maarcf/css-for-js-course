bd9aaf06c00cc2dc5ba4f6652de9effc5457faf8
maarcf <marr.cairo@gmail.com>
2024-08-10 16:48:09 -0300

    feat(): ex4

--- module-6/src/components/PhotoGridItem/PhotoGridItem.jsx
+++ module-6/src/components/PhotoGridItem/PhotoGridItem.jsx
@@ -50,17 +50,23 @@ const Image = styled.img`
 `;
 
 const Tags = styled.ul`
-  display: flex;
-  flex-wrap: wrap;
-  gap: 8px;
+  padding: 4px 0;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  white-space: nowrap;
 `;
 
 const Tag = styled.li`
+  display: inline;
   padding: 4px 8px;
   background: var(--color-gray-300);
   font-size: 0.875rem;
   font-weight: 475;
   color: var(--color-gray-800);
+
+  &:not(:last-child) {
+    margin-right: 8px;
+  }
 `;
 
 export default PhotoGridItem;
